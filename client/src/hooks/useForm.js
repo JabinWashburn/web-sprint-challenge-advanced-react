14faefc1789c3e44b38ba0f39be17e04860f5734
Jabin Washburn <jabinwashburn@gmail.com>
2020-08-07 11:32:05 -0700

    started new branch and got project up to date

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,8 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react'
+
+export const useForm = (key, initialForm) => {
+    const [form, setForm] = useState(() => {
+        
+    })
+}
