023e9e908697b7d91327d64b322592b780d72e45
Jabin Washburn <jabinwashburn@gmail.com>
2020-08-07 12:31:22 -0700

    got tests to work and sanity tested them to be sure

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,20 @@
 import React from "react";
-import { render } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import { render, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    const {getByText} = render(<CheckoutForm />)
+    const h2 = getByText(/checkout form/i)
+    expect(h2).toBeInTheDocument()
+})
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", async () => {
+    const { getByLabelText} = render(
+      <CheckoutForm />)
+      const firstName = getByLabelText(/first name/i);
+        await userEvent.type(firstName, "Jabin");
+        expect(firstName).toHaveValue("Jabin");
+})
